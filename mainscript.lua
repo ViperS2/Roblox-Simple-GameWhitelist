d3d286ee6ca936e5d914e8d28ebf6fc072830b0a
V <149577538+ViperS2@users.noreply.github.com>
2023-11-01 18:16:50 +0800

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -3,7 +3,7 @@ local Players = game:GetService("Players")
 -- Join to discord.gg/rippers
 -- Define a function to check if a player is whitelisted
 local function isPlayerWhitelisted(player)
-    local gameIdToCheck = "152177764033" -- Replace with the game ID you want to check
+    local gameIdToCheck = "YourGameID" -- Replace with the game ID you want to check
     local url = "https://website.com/whitelist.php?game_id=" .. gameIdToCheck
 
     local success, response = pcall(function()
